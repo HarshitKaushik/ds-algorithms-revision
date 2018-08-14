0f001289aaae7b04f1c9bfb94034c0ec1fba4ed6
Harshit Kaushik <harshit@kwench.in>
2018-08-14 22:17:19 +0530

    Find an element in sorted rotated array
    pivoted binary search in O(log n)

--- array/find-element-rotated-sorted-array.cpp
+++ array/find-element-rotated-sorted-array.cpp
@@ -1,45 +1,77 @@
 #include <iostream>
 using namespace std;
 
+// An element in a sorted array can be found in O(log n) time via binary search.
+// But suppose we rotate an ascending order sorted array at some pivot unknown to you beforehand.
+// So for instance, 1 2 3 4 5 might become 3 4 5 1 2.
+// Devise a way to find an element in the rotated array in O(log n) time.
+// https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
+
+// Function for binary search in O(log n) running time
+int binarySearch(int array[], int key, int low, int high) {
+  if (high < low) {
+    return -1;
+  }
+  int mid = (low + high) / 2;
+  if (key == array[mid]) {
+    return mid;
+  }
+  if (key > array[mid]) {
+    return binarySearch(array, (mid + 1), high, key);
+  }
+  return binarySearch(array, low, (mid - 1), key);
+}
+
+// function to find pivot in a sorted and rotated array
+// pivot is the index of the maximum value in the array
+int pivotSearch(int array[], int low, int high) {
+  // handle base cases
+  if (high < low) {
+    return -1;
+  }
+  if (low == high) {
+    return low;
+  }
+  int mid = low + high / 2;
+  if (mid < high && array[mid] > array[mid + 1]) {
+    return mid;
+  } else if (mid > low && array[mid - 1] > array[mid]) {
+    return mid - 1;
+  }
+  if (array[low] >= array[mid]) {
+    return pivotSearch(array, low, mid - 1);
+  }
+  return pivotSearch(array, mid + 1, high);
+}
+
+// Finds key in rotated sorted array[] in O(log n)
+int pivotedBinarySearch(int array[], int key, int size) {
+  int pivot = pivotSearch(array, 0, size - 1);
+
+  // If we didn't find a pivot,
+  // then array is not rotated at all
+  if (pivot == -1) {
+    return binarySearch(array, key, 0, size - 1);
+  }
+
+  // If we found a pivot, then first compare with pivot
+  // and then search in two subarrays around pivot
+  if (array[pivot] == key) {
+    return pivot;
+  }
+
+  if (array[0] <= key)
+    return binarySearch(array, key, 0, pivot - 1);
+
+  return binarySearch(array, key, pivot + 1, size - 1);
+}
+
 int main()
-{  
-  // Find an element in a rotated sorted array in O(log n) running time
-  // O(log n) complexity
-  // Assuming that all numbers here are different
-
-  // Input arr[] = {3, 4, 5, 1, 2}
-  // Element to Search = 1
-  //   1) Find out pivot point and divide the array in two
-  //       sub-arrays. (pivot = 2) /*Index of 5*/
-  //   2) Now call binary search for one of the two sub-arrays.
-  //       (a) If element is greater than 0th element then
-  //              search in left array
-  //       (b) Else Search in right array
-  //           (1 will go in else as 1 < 0th element(3))
-  //   3) If element is found in selected sub-array then return index
-  //      Else return -1.
-
-  int a[] = {4, 5, 6, 7, 8, 9, 10, 1, 2, 3};
-
-  // a[] of size 10
-  int n = 10;
-
-  // element to find
-  int element = 5;
-
-  int pivotIndex = -1;
-
-  for (int i = 0; i < n-1; i++) {
-    if (a[i] > a[i + 1]) {
-      pivotIndex = i;
-      break;
-    }
-  }
-  if (pivotIndex == -1) {
-    cout << "-1" << endl;
-    return;
-  }
-  if (a[pivotIndex] > )
-  
+{
+  int testArray[] = { 4,5,6,7,1,2,3 };
+  int size = 7;
+  int numberToFind = 5;
+  cout << pivotedBinarySearch(testArray, numberToFind, size) << endl;
+  // 1
   return 0;
 }
