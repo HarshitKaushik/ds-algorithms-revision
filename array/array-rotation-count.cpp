6b73604ca3c289820ac0c5a42dd0f028e2a8dc83
Harshit Kaushik <harshit@kwench.in>
2018-08-23 23:56:46 +0530

    Rotation count by modifying binary search in O(log n)

--- array/array-rotation-count.cpp
+++ array/array-rotation-count.cpp
@@ -11,6 +11,35 @@ void printArray(int arr[], int size)
   cout << endl;
 }
 
+// Finds the index of the minimum element in O(log n) running time
+// The minimum element is the only element whose previous is greater than it. If there is no previous element element, then there is no rotation (first element is minimum).
+// We check this condition for middle element by comparing it with (mid-1)’th and (mid+1)’th elements.
+// If minimum element is not at middle (neither mid nor mid + 1), then
+// minimum element lies in either left half or right half.
+int findMinimumElementInRotatedSortedArray(int arr[], int low, int high) {
+  // This condition is needed to handle the case
+  // when array is not rotated at all
+  if (high < low)
+    return 0;
+  // If there is only one element left
+  if (high == low)
+    return low;
+  // Find mid
+  int mid = (high + low) / 2;
+  // Check if element (mid + 1) is minimum element.
+  // Consider the cases like {3, 4, 5, 1, 2}
+  if (mid < high && arr[mid + 1] < arr[mid])
+    return (mid + 1);
+  // Check if mid itself is minimum element
+  if (mid > low && arr[mid] < arr[mid - 1])
+    return mid;
+  // Decide whether we need to go to left half or
+  // right half
+  if (arr[high] > arr[mid])
+    return findMinimumElementInRotatedSortedArray(arr, low, mid - 1);
+  return findMinimumElementInRotatedSortedArray(arr, mid + 1, high);
+}
+
 int main()
 {
   // test sorted array in ascending order right rotated by three places
@@ -27,8 +56,12 @@ int main()
       smallestElementIndex = i;
       break;
     }
-  cout << smallestElementIndex;
+  cout << smallestElementIndex << endl;
   // Output
   // 3
+
+  // We can also modify the binary search to find the answer in O(log n) time
+  int smallestElementIndexAgain = findMinimumElementInRotatedSortedArray(arr, 0 , n - 1);
+  cout << smallestElementIndexAgain << endl;
   return 0;
 }
