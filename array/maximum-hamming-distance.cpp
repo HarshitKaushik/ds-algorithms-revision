70d41e0af0997b48bc9d0d4ba63887fbd161d85b
Harshit Kaushik <harshit@kwench.in>
2018-09-01 22:52:24 +0530

    Rearrange an array such that a[i] = i or -1

--- array/maximum-hamming-distance.cpp
+++ array/maximum-hamming-distance.cpp
@@ -17,11 +17,11 @@ int maxHamming(int arr[], int n)
 {
   // arr[] to brr[] two times so that
   // we can traverse through all rotations.
-  int brr[2 * n + 1];
-  for (int i = 0; i < n; i++)
+  int brr[2 * n];
+  for (int i = 0; i < n; i++) {
     brr[i] = arr[i];
-  for (int i = 0; i < n; i++)
     brr[n + i] = arr[i];
+  }
 
   // We know hamming distance with 0 rotation
   // would be 0.
@@ -52,5 +52,7 @@ int main()
   int arr[] = {2, 4, 6, 8};
   int n = sizeof(arr) / sizeof(arr[0]);
   cout << maxHamming(arr, n);
+  // Output
+  // 4
   return 0;
 }
---
+++ array/rearrange-array-arr-i-equals-i.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+// Rearrange an array such that arr[i] = i
+// Given an array of elements of length N, ranging from 1 to N. All elements
+// may not be present in the array. If element is not present then there will
+// be -1 present in the array. Rearrange the array such that A[i] = i and if i
+// is not present, display -1 at that place.
+
+// Reference Link: https://www.geeksforgeeks.org/rearrange-array-arri/
+
+// Function to rearrange an array
+// such that arr[i] = i.
+int rearrangeArray(int array[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << "i => " << i << endl;
+    if (array[i] != -1 && array[i] != i)
+    {
+      cout << "array[i] => " << array[i] << endl;
+      int x = array[i];
+      cout << "x => " << x << endl;
+      // check if desired place
+      // is not vacated
+      while (array[x] != -1 && array[x] != x)
+      {
+        cout << "array[x] => " << array[x] << endl;
+        // store the value from
+        // desired place
+        int y = array[x];
+        // place the x to its correct
+        // position
+        array[x] = x;
+        // now y will become x, now
+        // search the place for x
+        x = y;
+        cout << "xAgain => " << x << endl;
+      }
+      // place the x to its correct
+      // position
+      cout << "x => " << x << endl;
+      array[x] = x;
+      // check if while loop hasn't
+      // set the correct value at A[i]
+      if (array[i] != i)
+      {
+        cout << "array[i] => " << array[i] << endl;
+        // if not then put -1 at
+        // the vacated place
+        array[i] = -1;
+      }
+    }
+  }
+}
+
+// Driver function.
+int main()
+{
+  int testArray[] = {-1, -1, 6, 1, 9, 3, 2, -1, 4, -1};
+  int len = sizeof(testArray) / sizeof(testArray[0]);
+  rearrangeArray(testArray, len);
+  for (int i = 0; i < len; i++)
+    cout << testArray[i] << " ";
+}
